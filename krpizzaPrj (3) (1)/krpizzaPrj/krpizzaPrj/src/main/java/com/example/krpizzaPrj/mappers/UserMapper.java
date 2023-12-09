c6e1e86a50a716576910870cc9211fd59105d9e0
tndals11 <136726862+tndals11@users.noreply.github.com>
2023-12-09 23:14:46 +0900

    Add files via upload

---
+++ krpizzaPrj (3) (1)/krpizzaPrj/krpizzaPrj/src/main/java/com/example/krpizzaPrj/mappers/UserMapper.java
@@ -0,0 +1,31 @@
+package com.example.krpizzaPrj.mappers;
+
+
+
+import com.example.krpizzaPrj.dto.UserDto;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+import java.util.Map;
+
+@Mapper
+public interface UserMapper {
+
+        @Insert("INSERT INTO users VALUES(NULL, #{userId}, #{userPasswd}, #{userEmail}, #{userName}, NOW(), NOW())")
+        public void setRegister(UserDto userDto);
+
+        @Select("SELECT COUNT(*) FROM users WHERE user_id = #{userId}")
+        public int getCheckUserId(String userId);
+
+        @Select("SELECT COUNT(*) FROM users WHERE user_email = #{userEmail}")
+        public int getCheckUserEmail(String userEmail);
+
+        @Select("SELECT users.user_Id FROM users WHERE user_name = #{userName} AND user_email = #{userEmail}")
+        public Map<String, Object> checkFindID(String userName, String userEmail);
+
+
+        @Select("SELECT COUNT(*) FROM users WHERE user_id = #{userId} AND user_pw = #{userPasswd} ")
+        public int checkLogin(String userId, String userPasswd);
+    }
