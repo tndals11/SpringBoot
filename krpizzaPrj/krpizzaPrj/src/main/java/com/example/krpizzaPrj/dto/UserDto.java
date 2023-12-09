e61471bfbd0dc66f39295c54f2802020887c0faf
tndals11 <136726862+tndals11@users.noreply.github.com>
2023-12-09 23:17:19 +0900

    Add files via upload

---
+++ krpizzaPrj/krpizzaPrj/src/main/java/com/example/krpizzaPrj/dto/UserDto.java
@@ -0,0 +1,68 @@
+package com.example.krpizzaPrj.dto;
+
+public class UserDto {
+    private int userNum;
+    private String userId;
+    private String userEmail;
+    private String userName;
+    private String userPasswd;
+    private String regDate;
+    private String endDate;
+
+    public int getUserNum() {
+        return userNum;
+    }
+
+    public void setUserNum(int userNum) {
+        this.userNum = userNum;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPasswd() {
+        return userPasswd;
+    }
+
+    public void setUserPasswd(String userPasswd) {
+        this.userPasswd = userPasswd;
+    }
+
+    public String getRegDate() {
+        return regDate;
+    }
+
+    public void setRegDate(String regDate) {
+        this.regDate = regDate;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+}
+
