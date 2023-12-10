3f5c0f388dd79da2c7c5c4c4c39c1f5e05c10405
tndals11 <136726862+tndals11@users.noreply.github.com>
2023-12-11 08:59:58 +0900

    20231211 비밀번호 찾기 작업 완료 (#3)

--- korpizzaPrj/korpizzaPrj/src/main/java/com/example/krpizzaPrj/controller/UserController.java
+++ korpizzaPrj/korpizzaPrj/src/main/java/com/example/krpizzaPrj/controller/UserController.java
@@ -3,77 +3,72 @@ package com.example.krpizzaPrj.controller;
 
 import com.example.krpizzaPrj.dto.UserDto;
 import com.example.krpizzaPrj.mappers.UserMapper;
-import jakarta.servlet.http.HttpSession;
-import jakarta.websocket.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
 public class UserController {
 
-        @Autowired
-        UserMapper userMapper;
+    @Autowired
+    UserMapper userMapper;
 
 
-        @GetMapping("/common/register")
-        public String getRegister() { 
+    @GetMapping("/common/register")
+    public String getRegister() {
 
-            return "common/register";
+        return "common/register";
+    }
+    // 회원가입 페이지
+    @PostMapping("/common/register")
+    @ResponseBody
+    public Map<String, Object> setRegister(@ModelAttribute UserDto userDto) {
+        System.out.println(userDto);
+        Map<String, Object> map = new HashMap<>();
+        if( userDto != null ) {
+            userMapper.setRegister(userDto);
+            map.put("msg", "회원가입이 완료되었습니다.");
         }
+        return map;
+    }
 
-        @PostMapping("/common/register")
-        @ResponseBody
-        public Map<String, Object> setRegister(@ModelAttribute UserDto userDto) {
-            System.out.println(userDto);
-            Map<String, Object> map = new HashMap<>();
-            if( userDto != null ) {
-                userMapper.setRegister(userDto);
-                map.put("msg", "회원가입이 완료되었습니다.");
-            }
-            return map;
-        }
+    // 아이디 중복성 체크
+    @GetMapping("/common/checkUserId")
+    @ResponseBody
+    public Map<String, Object> getCheckUserId(@RequestParam String userId) {
+        int CheckUserId = userMapper.getCheckUserId(userId);
 
-        @GetMapping("/common/checkUserId")
-        @ResponseBody
-        public Map<String, Object> getCheckUserId(@RequestParam String userId) {
-            int CheckUserId = userMapper.getCheckUserId(userId);
+        return Map.of("checkUserId", CheckUserId);
+    }
 
-            return Map.of("checkUserId", CheckUserId);
-        }
+    // 이메일 중복성 체크
+    @GetMapping("/common/checkUserEmail")
+    @ResponseBody
+    public Map<String, Object> getCheckUserEmail(@RequestParam String userEmail) {
+        int CheckUserEmail = userMapper.getCheckUserEmail(userEmail);
 
-        @GetMapping("/common/checkUserEmail")
-        @ResponseBody
-        public Map<String, Object> getCheckUserEmail(@RequestParam String userEmail) {
-            int CheckUserEmail = userMapper.getCheckUserEmail(userEmail);
+        return Map.of("CheckUserEmail", CheckUserEmail);
+    }
 
-            return Map.of("CheckUserEmail", CheckUserEmail);
-        }
+    @GetMapping("/common/findID")
+    public String getFindID() {
 
-        @GetMapping("/common/findID")
-        public String getFindID() {
-
-            return "common/findID";
-        }
-        @GetMapping("/common/findPw")
-        public String getFindPw() {
-
-            return "common/findPw";
-        }
+        return "common/findID";
+    }
 
 
+    // 로그인 페이지 
+    @GetMapping("/common/login")
+    public String getLogin() {
 
-        @GetMapping("/common/login")
-        public String getLogin() {
-
-            return "common/login";
-        }
-
+        return "common/login";
+    }
+    
+    // 로그인 페이지 값을 받아와 처리 
     @PostMapping("/common/checkLogin")
     @ResponseBody
     public String checkLogin(@RequestBody UserDto userDto) { // Model <- 백단  // @ModelAttribute 뷰단 -> 백단
@@ -90,34 +85,63 @@ public class UserController {
         System.out.println(msg);
         return msg;
     }
+    
+    // 아이디 찾기 페이지 값을 받아와 처리
+    @PostMapping("/common/checkFindID")
+    @ResponseBody
+    public Map<String, Object> checkFindID(@RequestBody UserDto userDto) { // Model <- 백단  // @ModelAttribute 뷰단 -> 백단
 
-        @PostMapping("/common/checkFindID")
-        @ResponseBody
-        public Map<String, Object> checkFindID(@RequestBody UserDto userDto) { // Model <- 백단  // @ModelAttribute 뷰단 -> 백단
-            Map<String, Object> map = new HashMap<>();
-            UserDto result = userMapper.checkFindID(userDto);
-            System.out.println(userDto);
-
-            String msg = "";
-            if ( result != null) {
-                msg = "success";
-            } else {
-                msg = "fail";
-            }
-
-            map.put("msg", msg); // --> 메세지를 보낼려고 쓴것
+        UserDto result = userMapper.checkFindID(userDto);
+        Map<String, Object> map = new HashMap<>();
+        String msg = "";
+        if ( result != null ) {
+            msg = "success";
             map.put("userId", result.getUserId() ); // url 담을려고 변수명 userId에 getUserId를 받아옴
-
-            return map;
+        } else {
+            msg = "fail";
         }
-
+        map.put("msg", msg); // --> 메세지를 보낼려고 쓴것
+        return map;
+    }
+    
+    // 아이디 찾기 후 뷰단에 보여주는 페이지
     @GetMapping("/user/viewFindId")
     public String viewFindId(@RequestParam String userId, Model model) {
-        System.out.println("userId : " + userId);
         model.addAttribute("userId", userId);
         return "user/viewFindId";
     }
 
+    // 비밀번호 찾기 주소
+    @GetMapping("/common/findPw")
+    public String getFindPw() {
+
+        return "common/findPw";
+    }
+
+    // 비밀번호 찾기 주소 값 받기
+    @PostMapping("/common/checkFindPw")
+    @ResponseBody
+    public Map<String, Object> checkFindPw(@RequestBody UserDto userDto) { // Model <- 백단  // @ModelAttribute 뷰단 -> 백단
+        UserDto res = userMapper.checkFindPw(userDto);
+        Map<String, Object> map = new HashMap<>();
+        String msg = "";
+        if ( res != null ) {
+            msg = "success";
+            map.put("userPw", res.getUserPasswd() ); // url 담을려고 변수명 userId에 getUserId를 받아옴
+        } else {
+            msg = "fail";
+        }
+        map.put("msg", msg); // --> 메세지를 보낼려고 쓴것
+        return map;
+    }
+    
+    // 비밀번호 찾기 후 뷰단에 보여주는 페이지 
+    @GetMapping("/common/viewFindPw")
+    public String viewFindPw(@RequestParam String userPw, Model model) {
+        System.out.println("userPw : " + userPw);
+        model.addAttribute("userPw", userPw);
+        return "common/viewFindPw";
+    }
 
     @GetMapping("/common/mainPage")
     public String getMainPage() {
@@ -148,5 +172,5 @@ public class UserController {
             return "common/original";
         }
 
-        // 테스트
+
 }
--- korpizzaPrj/korpizzaPrj/src/main/java/com/example/krpizzaPrj/mappers/UserMapper.java
+++ korpizzaPrj/korpizzaPrj/src/main/java/com/example/krpizzaPrj/mappers/UserMapper.java
@@ -11,20 +11,30 @@ import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
-
+        
+        // 회원가입
         @Insert("INSERT INTO users VALUES(NULL, #{userId}, #{userPasswd}, #{userEmail}, #{userName}, NOW(), NOW())")
         public void setRegister(UserDto userDto);
-
+        
+        // 아이디 중복 체크
         @Select("SELECT COUNT(*) FROM users WHERE user_id = #{userId}")
         public int getCheckUserId(String userId);
-
+        
+        // 이메일 중복 체크
         @Select("SELECT COUNT(*) FROM users WHERE user_email = #{userEmail}")
         public int getCheckUserEmail(String userEmail);
 
+        // 아이디 찾기
         @Select("SELECT users.user_id FROM users WHERE user_name = #{userName} AND user_email = #{userEmail}")
         public UserDto checkFindID( UserDto userDto );
 
-        @Select("SELECT  user_id as userId, user_pw as userPasswd FROM users WHERE  user_id = #{userId} AND user_pw = #{userPasswd}")
+        // 비밀번호 찾기
+        // #userPasswd# userPasswd가 없어서 오류 계속 발생!! mybatis 이름 변환이 달라서 그런것 같다
+        @Select("SELECT users.user_pw as userPasswd FROM users WHERE user_id = #{userId} AND user_email = #{userEmail}")
+        public UserDto checkFindPw( UserDto userDto);
+
+        // 로그인
+        @Select("SELECT user_id as userId, user_pw as userPasswd FROM users WHERE  user_id = #{userId} AND user_pw = #{userPasswd}")
         public UserDto checkLogin(UserDto userDto);
 
     }
